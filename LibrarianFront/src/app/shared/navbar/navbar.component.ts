cb894db1d7c71380f59ae5d83fac40ef03314214
OPTIMISSA\johansel.calderon <johansel.calderon@hotmail.com>
2022-08-11 12:25:28 +0200

    [#27361] actualizacion log out

--- LibrarianFront/src/app/shared/navbar/navbar.component.ts
+++ LibrarianFront/src/app/shared/navbar/navbar.component.ts
@@ -1,39 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { UsersService } from '../../home/services/users.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
   showHiddenOptions: boolean = false;
 
-  constructor(private usuarios : UsersService, private router: Router) { }
+  constructor(private usuarios: UsersService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  logout() {
+    Swal.fire({
+      title: 'Estas seguro de cerrar sesión?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'SI!',
+      cancelButtonText: 'NO'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Sesión Cerrada', 'Cerrada', 'success');
+        this.usuarios.logOut();
+        this.showHiddenOptions = false;
+        this.router.navigate(['']);
+      }
+    });
   }
 
-  logout(){
-    this.usuarios.logOut();
-    this.showHiddenOptions = false;
-    this.router.navigate([""]);
-  }
-
-  isLogged(){
+  isLogged() {
     return this.usuarios.isLogged();
   }
 
-  goHomeOrLogin(){
+  goHomeOrLogin() {
     this.showHiddenOptions = false;
     if (this.isLogged()) {
-      this.router.navigate(["/"]);
-    } else{
-      this.router.navigate(["/"]);
+      this.router.navigate(['/']);
+    } else {
+      this.router.navigate(['/']);
     }
   }
-
-
 }
