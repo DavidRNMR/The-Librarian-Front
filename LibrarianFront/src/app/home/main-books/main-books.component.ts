471c748e1f7935dbb8bb14479c0ba5e75d0e90db
OPTIMISSA\raul.gonzalez <raul.gonzalez@optimissa.com>
2022-08-19 11:20:52 +0200

    ﻿[#27480] Creado modal en bucle

--- LibrarianFront/src/app/home/main-books/main-books.component.ts
+++ LibrarianFront/src/app/home/main-books/main-books.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../services/book.service';
 import { Books, VolumeInfo, Item } from '../interfaces/books';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main-books',
@@ -8,19 +9,46 @@ import { Books, VolumeInfo, Item } from '../interfaces/books';
   styleUrls: ['./main-books.component.css'],
 })
 export class MainBooksComponent implements OnInit {
+  books: Item[] = [];
+  public load: boolean;
+  showModal!: boolean;
+  title = 'appBootstrap';
+  closeResult!: string;
 
-  books: Item[]=[];
-
+  constructor(private bookService: BookService, private modalService: NgbModal) {
 
   constructor(private bookService: BookService) { }
 
-  ngOnInit(): void {
-
-    this.getBooks();
-    
+  open(content:any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
+  show() {
+    this.showModal = true; // Show-Hide Modal Check
+  }
+  //Bootstrap Modal Close event
+  hide() {
+    this.showModal = false;
+  }
 
   getBooks() {
     this.bookService.getRandomBooks().subscribe((data) => {
