d9d2c6c36b8379c6026ad24642830d3779682423
OPTIMISSA\johansel.calderon <johansel.calderon@hotmail.com>
2022-08-25 09:35:27 +0200

    [#27562] Modificado modal y navbar

--- LibrarianFront/src/app/home/components/main-card/main-card.component.ts
+++ LibrarianFront/src/app/home/components/main-card/main-card.component.ts
@@ -6,18 +6,19 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-main-card',
   templateUrl: './main-card.component.html',
-  styleUrls: ['./main-card.component.css']
+  styleUrls: ['./main-card.component.css'],
 })
 export class MainCardComponent implements OnInit {
-
   title = 'appBootstrap';
 
   closeResult!: string;
 
-  @Input() books: Item[]=[];
+  @Input() books: Item[] = [];
 
-  constructor(private modalService: NgbModal,
-	      public translate: TranslateService) {
+  constructor(
+    private modalService: NgbModal,
+    public translate: TranslateService
+  ) {
     // Register translation languages
     translate.addLangs(['es', 'en', 'fr', 'de']);
     // Set default language
@@ -29,53 +30,31 @@ export class MainCardComponent implements OnInit {
     this.translate.use(lang);
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
-
-  open(content:any) {
-
+  open(content: any) {
     this.modalService
 
       .open(content, { ariaLabelledBy: 'modal-basic-title' })
 
       .result.then(
-
         (result) => {
-
           this.closeResult = `Closed with: ${result}`;
-
         },
 
         (reason) => {
-
           this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-
         }
-
       );
-
   }
-
-
-
 
   private getDismissReason(reason: any): string {
-
     if (reason === ModalDismissReasons.ESC) {
-
       return 'by pressing ESC';
-
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-
       return 'by clicking on a backdrop';
-
     } else {
-
       return `with: ${reason}`;
-
     }
-
   }
-
 }
