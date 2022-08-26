3882a7aedc0b4afd3348053f6c81654d3f85eeac
OPTIMISSA\johansel.calderon <johansel.calderon@hotmail.com>
2022-08-26 09:46:39 +0200

    implemantacion de descargar pdf por usuario logueado funcionando

--- LibrarianFront/src/app/home/services/reserve.service.ts
+++ LibrarianFront/src/app/home/services/reserve.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
-const HOST = "http://localhost:8080/reserve/updateReserve";
+// const HOST = "http://localhost:8080/reserve/updateReserve";
 
 @Injectable({
   providedIn: 'root'
@@ -37,7 +37,16 @@ export class ReserveService {
   }
 
   updateReserve(reserva: any, id:number ){
-    return this.http.put(`${HOST}/${id}`,reserva);
+    return this.http.put(`${this.URLaddReserve}/updateReserve/${id}`,reserva);
+  }
+
+  downloadPdf(id:number):Observable<any>{
+    // Pendiente para la implementacion
+    // hay que usar las siguientes instalaciones
+    //  ejemplo https://roytuts.com/download-file-from-server-using-angular/
+    // npm install file-saver --save
+    // npm install -D @types/file-saver
+    return this.http.get(`${this.URLaddReserve}/export/pdf/${id}`)
   }
 
 
